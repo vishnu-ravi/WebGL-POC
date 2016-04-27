e0da63ebb165bbdf8337c1284c24ddbdca772dab
vishnu-ravi <balavishnu.r@krds.fr>
2016-04-27 16:46:38 +0530

    pinch correct

--- public/javascript/globe.js
+++ public/javascript/globe.js
@@ -232,11 +232,11 @@ DAT.Globe = function(container, colorFn) {
     container.addEventListener('mousedown', onMouseDown, false);
     container.addEventListener('mousewheel', onMouseWheel, false);
     hammertime.on('pinchin', function(e) {
-        zoom(-10);
+        zoom(-20);
     });
 
     hammertime.on('pinchout', function(e) {
-        zoom(10);
+        zoom(20);
     });
 
     document.addEventListener('keydown', onDocumentKeyDown, false);
